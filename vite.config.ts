1b52470dda05098e6a1008a9e338be794e67deeb
Sander Moolin <sandermoolin@gmail.com>
2021-10-01 14:01:31 -0400

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,19 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+    plugins: [vue()],
+    build: {
+        lib: {
+            entry: './src/canvas-everything/index.ts',
+            name: 'canvas-everything',
+        },
+        rollupOptions: {
+            external: ['vue'],
+            output: {
+                globals: {
+                    vue: 'Vue',
+                }
+            }
+        }
+    }
 })
