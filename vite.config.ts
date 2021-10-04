c1d9a5301b5d9aa88b38a2a7ea43c245da206b97
Sander Moolin <sandermoolin@gmail.com>
2021-10-04 10:15:30 -0400

    update types

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dts from 'vite-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [dts(), vue()],
     build: {
         lib: {
             entry: './src/canvas-everything/index.ts',
