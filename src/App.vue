bbf01555d736b8953d4fd3efb88cf9ca95c7aca2
Sander Moolin <sandermoolin@gmail.com>
2021-10-05 09:43:32 -0400

    wip on splitting directive

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <main class="canvas-everything-demo">
-        <h1>Title</h1>
-        <p>
+        <!-- <h1>Title</h1> -->
+        <!-- <p>
             <span class="p1" v-canvas>nonne inventa sunt?</span> Quis est, qui
             non oderit libidinosam, protervam adolescentiam? Verum tamen cum de
             rebus grandioribus dicas, ipsae res verba rapiunt; Quibusnam
@@ -11,27 +11,35 @@
             Illud dico, ea, quae dicat, praeclare inter se cohaerere. Cur
             igitur, cum de re conveniat, non malumus usitate loqui? Quae cum
             dixisset paulumque institisset, Quid est?
-        </p>
+        </p> -->
         <div class="image-sizer">
             <img src="//placekitten.com/250/250" v-canvas />
         </div>
-        <h2 v-canvas>Another Title</h2>
-        <p>
-            <strong>Lorem ipsum dolor sit amet,</strong> consectetur adipiscing
-            elit. Ea, quae dialectici nunc tradunt et docent, nonne ab illis
-            instituta sunt aut inventa sunt? Quis est, qui non oderit
-            libidinosam, protervam adolescentiam? Verum tamen cum de rebus
-            grandioribus dicas, ipsae res verba rapiunt; Quibusnam praeteritis?
-            Duo Reges: constructio interrete. Cur deinde Metrodori liberos
-            commendas? Se dicere inter honestum et turpe nimium quantum, nescio
-            quid inmensum, inter ceteras res nihil omnino interesse. Illud dico,
-            ea, quae dicat, praeclare inter se cohaerere. Cur igitur, cum de re
-            conveniat, non malumus usitate loqui? Quae cum dixisset paulumque
-            institisset, Quid est?
-        </p>
+        <!-- <h2>Another Title</h2> -->
+        <div v-canvas>
+            Text
+            <p class="test">
+                <strong>Lorem ipsum dolor sit amet,</strong> consectetur
+                adipiscing elit. Ea, quae dialectici nunc tradunt et docent,
+                nonne ab illis instituta sunt aut inventa sunt? Quis est, qui
+                non oderit libidinosam, protervam adolescentiam? Verum tamen cum
+                de rebus grandioribus dicas, ipsae res verba rapiunt; Quibusnam
+                praeteritis? Duo Reges: constructio interrete. Cur deinde
+                Metrodori liberos commendas? Se dicere inter honestum et turpe
+                nimium quantum, nescio quid inmensum, inter ceteras res nihil
+                omnino interesse. Illud dico, ea, quae dicat, praeclare inter se
+                cohaerere. Cur igitur, cum de re conveniat, non malumus usitate
+                loqui? Quae cum dixisset paulumque institisset, Quid est?
+            </p>
+
+            <ul>
+                <li>Example</li>
+                <li>Second Example</li>
+            </ul>
+        </div>
 
         <h2 class="color-title" v-canvas>Color Title</h2>
-        <p>
+        <p v-canvas>
             <strong>Lorem ipsum dolor sit amet,</strong> consectetur adipiscing
             elit. Ea, quae dialectici nunc tradunt et docent, nonne ab illis
             instituta sunt aut inventa sunt? Quis est, qui non oderit
@@ -44,19 +52,22 @@
             conveniat, non malumus usitate loqui? Quae cum dixisset paulumque
             institisset, Quid est?
         </p>
-        <h1>Title</h1>
+        <h1 v-canvas>Title</h1>
     </main>
 </template>
+
 
 <style lang="scss">
 .canvas-everything-demo {
     font-family: sans-serif;
+    text-align: center;
 
     h2 {
         font-weight: 400;
     }
     p {
         max-width: 400px;
+        margin: 20px auto;
     }
     .p1 {
         background: blue;
--- src/canvas-everything/directive/index.ts
+++ src/canvas-everything/directive/index.ts
@@ -6,6 +6,7 @@ import {
     intersectionObserver,
     removeCanvasEverythingNode,
 } from '../core'
+import { getTextNodesIn } from './getTextNodesInElement'
 
 export const directive: ObjectDirective = {
     mounted(el: HTMLElement, binding) {
@@ -15,29 +16,32 @@ export const directive: ObjectDirective = {
         const uuid = options.uuid ?? createUuid()
         el.setAttribute(canvasEverythingUuidAttribute, uuid)
 
-        // create node
-        addOrUpdateCanvasEverythingNode({
-            element: el,
-            focus: false,
-            hover: false,
-            isIntersecting: false,
-            meta: binding.value?.meta,
-            rect: el.getBoundingClientRect(),
-            refresh() {
-                this.rect = this.element.getBoundingClientRect()
-                this.style = window.getComputedStyle(this.element)
-            },
-            style: window.getComputedStyle(el),
-            type: 'CanvasEverything',
-            updateOverride: undefined,
-            uuid,
-            z: 0,
-            ...options,
-        })
-
         // hide original el
         el.style.opacity = '0'
 
+        // find all text nodes
+        const textNodes = getTextNodesIn(el)
+        // replace each text node with text split into spans
+        const container = document.createElement('div')
+        textNodes.forEach(node => {
+            const newContent = '<span data-canvas-everything-wrapper><span>' + (node.textContent ?? '').trim().replace(/\s+/g, '</span> <span>') + '</span></span>'
+            container.innerHTML = newContent
+            node.parentElement?.replaceChild(container.firstChild!, node)
+        })
+
+        // add new elements
+        const toTrack = Array.from(el.querySelectorAll('*[data-canvas-everything-wrapper] > span'))
+        if (toTrack.length) {
+            toTrack.forEach((item, i) => {
+                const itemUuid = `${uuid}.${i}`
+                item.setAttribute(canvasEverythingUuidAttribute, itemUuid)
+
+                addOrUpdate(item as HTMLElement, binding, itemUuid, options)
+            })
+        } else {
+            // const target = toTrack.lengt
+            addOrUpdate(el as HTMLElement, binding, uuid, options)
+        }
     },
     unmounted(el: HTMLElement) {
         const uuid = el.getAttribute(canvasEverythingUuidAttribute)
@@ -46,4 +50,27 @@ export const directive: ObjectDirective = {
             intersectionObserver.unobserve(el)
         }
     }
+}
+
+
+const addOrUpdate = (el: HTMLElement, binding: any, uuid: CanvasEverything.Uuid, options: CanvasEverything.DirectiveOptions) => {
+    // create node
+    addOrUpdateCanvasEverythingNode({
+        element: el,
+        focus: false,
+        hover: false,
+        isIntersecting: false,
+        meta: binding.value?.meta,
+        rect: el.getBoundingClientRect(),
+        refresh() {
+            this.rect = this.element.getBoundingClientRect()
+            this.style = window.getComputedStyle(this.element)
+        },
+        style: window.getComputedStyle(el),
+        type: 'CanvasEverything',
+        updateOverride: undefined,
+        uuid,
+        z: 0,
+        ...options,
+    })
 }
